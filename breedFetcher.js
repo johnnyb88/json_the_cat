17cedff25e5fb29f6d741c5ef61b57ea48e837d3
johnnyb88 <johnnybarratt@gmail.com>
2019-09-26 20:21:56 +0000

    refractored breed test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,40 @@
 const request = require('request');
-let input = process.argv.slice(2);
-let breed = input[0];
 let catApi = 'https://api.thecatapi.com/v1/breeds/search?q=';
 // API URL/endpoint which will return cat breed results.
 
-request(catApi + breed, (error, response, body) => {
-  if (error) {
-    if (error.code !== undefined) {
-      console.log("Error, request failed");
-      throw error;
+const fetchBreedDescription = function(breedName, callback) {
+  request(catApi + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      error = "Breed name not found";
+      callback(error);
+    } else {
+      callback(error, data[0]['description']);
     }
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    return console.log("Breed name not found");
-  }
-  console.log(data[0]['description']);
-});
+  });
+  
+};
 
 
 
+module.exports = { fetchBreedDescription };
 
+// const request = require('request');
+// let input = process.argv.slice(2);
+// let breed = input[0];
+// let catApi = 'https://api.thecatapi.com/v1/breeds/search?q=';
+// // API URL/endpoint which will return cat breed results.
+
+// request(catApi + breed, (error, response, body) => {
+//   if (error) {
+//     if (error.code !== undefined) {
+//       console.log("Error, request failed");
+//       throw error;
+//     }
+//   }
+//   const data = JSON.parse(body);
+//   if (data[0] === undefined) {
+//     return console.log("Breed name not found");
+//   }
+//   console.log(data[0]['description']);
+// });
